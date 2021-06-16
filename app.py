46f495f0be2df2b4a7cec85e3bb497682769ad80
someshfengde <someshfengde@gmail.com>
2021-06-17 01:13:34 +0530

    added functionality
    this is it maybe

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import functions as fn
-import numpy as np
-import pandas as pd
 
 st.title('Text poetry generator')
 
@@ -10,6 +8,7 @@ st.write('enter the text data to get our model started ')
 scentence = st.text_input('Input your text here')
 
 if scentence:
+    st.title('Cooking some stuff')
     predictions = fn.get_prediction(scentence)
 else:
     st.write('enter the valid text input')
--- functions.py
+++ functions.py
@@ -1,9 +1,28 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-import tensorflow as tf
+from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
+
+
+def get_token(path):
+    load_token = GPT2Tokenizer.from_pretrained(path)
+    return load_token
+
+
+def load_model(path):
+    load_model = TFGPT2LMHeadModel.from_pretrained(path)
+    return load_model
+
+
+def make_predictions(scentence):
+    tokenizer = get_token('./tokenizer')
+    tokens = tokenizer.encode(scentence, return_tensors='tf')
+    model = load_model('./model')
+    output = model.generate(tokens,
+                            max_length=1000,
+                            top_k=30,
+                            num_beams=5)
+    actual_scentence = tokenizer.decode(output[0], skip_special_tokens=True)
+    return actual_scentence
 
 
 def get_prediction(scentence):
-
-    return scentence
+    answer = make_predictions(scentence)
+    return answer
